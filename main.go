1e4c86a0402e4d659a842c805888ec678ddae5cb
Rahul-aithal <aithalrahul34@gmail.com>
2026-05-07 18:13:11 +0530

    refactor: clean up Google Calendar MCP server implementation

--- main.go
+++ main.go
@@ -22,17 +22,20 @@ type AddEventMCP struct {
 	StartDateTime time.Time `json:"start_date_time"`
 	EndDateTime   time.Time `json:"end_date_time"`
 }
-type ListEventsMcp struct {
-	Limit int `json:"list"`
+type ListEventsMCP struct {
+	Limit int `json:"limit"`
 }
-type DeleteEventMcp struct {
-	Name    string
-	MaxDate time.Time
-	MinDate time.Time
+type DeleteEventMCP struct {
+	Name    string    `json:"name"`
+	MaxDate time.Time `json:"max_date"`
+	MinDate time.Time `json:"min_date"`
 }
 
 var config *oauth2.Config
 
+// ====================
+// MCP Tool Definitions
+// ====================
 func addEvent(ctx context.Context, req *mcp.CallToolRequest, input AddEventMCP) (*mcp.CallToolResult, any, error) {
 	log.Println("Add event Started")
 	event := &calendar.Event{
@@ -46,202 +49,226 @@ func addEvent(ctx context.Context, req *mcp.CallToolRequest, input AddEventMCP) 
 			TimeZone: "Asia/Kolkata",
 		},
 	}
-	creatEvent(ctx, event)
-	eventReulst := fmt.Sprintf("Event created: %s\n", event.HtmlLink)
+	err := createEvent(ctx, event)
+	if err != nil {
+		return nil, nil, err
+	}
+	eventResult := "Event created: " + event.HtmlLink
 	return &mcp.CallToolResult{
-		Content: []mcp.Content{&mcp.TextContent{Text: eventReulst}},
+		Content: []mcp.Content{&mcp.TextContent{Text: eventResult}},
 	}, nil, nil
 }
 
-func listEventsByLimit(ctx context.Context, req *mcp.CallToolRequest, input ListEventsMcp) (*mcp.CallToolResult, any, error) {
+func listUpcomingEventsByLimit(ctx context.Context, req *mcp.CallToolRequest, input ListEventsMCP) (*mcp.CallToolResult, any, error) {
 	log.Println("Listing events")
 	if input.Limit < 1 {
-		log.Fatal("0 limit is not allowed")
+
+		return nil, nil, fmt.Errorf("limit must be greater than 0")
 	}
-	events := listEvents(ctx, input.Limit)
-	var builder strings.Builder
+	events, err := listUpcomingEvents(ctx, input.Limit)
+
+	if err != nil {
+		return nil, nil, err
+	}
+	if len(events) == 0 {
+		return &mcp.CallToolResult{
+			Content: []mcp.Content{
+				&mcp.TextContent{
+					Text: "No upcoming events found.",
+				},
+			},
+		}, nil, nil
+	}
+	var eventText strings.Builder
 
 	for i, event := range events {
-		fmt.Fprintf(&builder, "%d. %s\n", i+1, event)
+		fmt.Fprintf(&eventText, "%d. %s\n", i+1, event)
 	}
 
-	event_text := builder.String()
 	return &mcp.CallToolResult{
-		Content: []mcp.Content{&mcp.TextContent{Text: event_text}},
+		Content: []mcp.Content{&mcp.TextContent{Text: eventText.String()}},
 	}, nil, nil
 }
 
-func deleteEventByName(ctx context.Context, req *mcp.CallToolRequest, input DeleteEventMcp) (*mcp.CallToolResult, any, error) {
-	log.Println("Listing events")
-	result := deleteEvent(ctx, input)
+func deleteEventByName(ctx context.Context, req *mcp.CallToolRequest, input DeleteEventMCP) (*mcp.CallToolResult, any, error) {
+	log.Println("Deleting events")
+	result, err := deleteEvent(ctx, input)
+
+	if err != nil {
+		return nil, nil, err
+	}
 	resp := input.Name
 	if result {
 		resp = resp + " successfully deleted\n"
 	} else {
-		resp = resp + " couldn't not be deleted\n"
+		resp = resp + " could not be deleted\n"
 	}
 	return &mcp.CallToolResult{
 		Content: []mcp.Content{&mcp.TextContent{Text: resp}},
 	}, nil, nil
 }
 
-func creatEvent(ctx context.Context, event *calendar.Event) {
+// ====================
+// Google Calendar Logic
+// ====================
 
+func createEvent(ctx context.Context, event *calendar.Event) error {
 	calendarId := "primary"
 	calendarService, err := createCalService(ctx)
 	//
 	if err != nil {
-		log.Fatalf("Error while creating calendar service")
+		return fmt.Errorf("error while creating calendar service: %w", err)
 	}
 	event, err = calendarService.Events.Insert(calendarId, event).Do()
 	if err != nil {
-		log.Fatalf("Unable to create event. %v\n", err)
+		return fmt.Errorf("unable to create event: %w", err)
 	}
 
 	log.Printf("Event Created from calendar:%v\n", event.HtmlLink)
-
+	return nil
 }
-func deleteEvent(ctx context.Context, event_data DeleteEventMcp) bool {
-
+func deleteEvent(ctx context.Context, eventData DeleteEventMCP) (bool, error) {
 	calendarId := "primary"
 	calendarService, err := createCalService(ctx)
 
 	if err != nil {
-		log.Fatalf("Error while creating calendar service")
-		return false
+		return false, fmt.Errorf("error while creating calendar service: %w", err)
 	}
 	events, err := calendarService.Events.List("primary").ShowDeleted(false).
-		SingleEvents(true).TimeMin(event_data.MinDate.Format(time.RFC3339)).TimeMax(event_data.MaxDate.Format(time.RFC3339)).
-		MaxResults(1).OrderBy("startTime").Q(event_data.Name).Do()
+		SingleEvents(true).TimeMin(eventData.MinDate.Format(time.RFC3339)).TimeMax(eventData.MaxDate.Format(time.RFC3339)).
+		MaxResults(1).OrderBy("startTime").Q(eventData.Name).Do()
 
 	if err != nil {
-		log.Fatalf("Error while searching calendar event ")
+		return false, fmt.Errorf("error while searching calendar event: %w", err)
+	}
+	if len(events.Items) == 0 {
+		return false, fmt.Errorf("no items found")
 	}
 	err = calendarService.Events.Delete(calendarId, events.Items[0].Id).Do()
 	if err != nil {
-		log.Fatalf("Unable to delete a event. %v\n", err)
-		return false
+		return false, fmt.Errorf("unable to delete event: %w", err)
 	}
 
 	log.Printf("Event Delete from calendar:%v\n", events.Items[0].Summary)
 
-	return true
-}
-func login(ctx context.Context) (*oauth2.Token, error) {
-	configdir, _ := os.UserConfigDir()
-	refresh_token_path := filepath.Join(configdir, "sechduler", "refresh_token.txt")
-	path := filepath.Join(configdir, "sechduler")
-	_, err := os.Stat(path)
-
-	if err != nil {
-		err := os.Mkdir(path, 0755)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-	}
-
-	refe_token_data, rerr := os.ReadFile(refresh_token_path)
-
-	if rerr == nil {
-
-		log.Printf("Reading the tokens form file\n refresh_token: (%s)\n", string(refe_token_data))
-
-		initToken := &oauth2.Token{
-			RefreshToken: string(refe_token_data),
-		}
-		ts := config.TokenSource(ctx, initToken)
-		token, err := ts.Token()
-		if err != nil {
-			log.Fatal(err)
-			return nil, err
-		}
-		// fmt.Printf("New token found %s\n", token.AccessToken)
-		// fmt.Printf("New token found %v\n", token.ExpiresIn)
-
-		rerr := os.WriteFile(refresh_token_path, []byte(token.RefreshToken), 0644)
-		if rerr != nil {
-			log.Fatal(rerr)
-		}
-		log.Printf("Logged In sucessfully")
-		return token, nil
-	}
-
-	url := config.AuthCodeURL("state")
-	log.Printf("%s", url)
-
-	log.Println("Paste the code from browser: ")
-	var code string
-	fmt.Scan(&code)
-	token, err := config.Exchange(ctx, code)
-	if err != nil {
-		return nil, err
-	}
-	rerr = os.WriteFile(refresh_token_path, []byte(token.RefreshToken), 0644)
-	if rerr != nil {
-		log.Fatal(rerr)
-	}
-	return token, nil
+	return true, nil
 }
 
-func createCalService(ctx context.Context) (*calendar.Service, error) {
-	token, err := login(ctx)
-	if err != nil {
-		log.Fatal(err)
-		return nil, err
-	}
-	calendarService, err := calendar.NewService(ctx, option.WithTokenSource(config.TokenSource(ctx, token)))
-	//
-	if err != nil {
-		log.Fatalf("Error while creating calendar service")
-		return nil, err
-	}
-	return calendarService, nil
-}
-
-func listEvents(ctx context.Context, limit int) []string {
-
+func listUpcomingEvents(ctx context.Context, limit int) ([]string, error) {
 	t := time.Now().Format(time.RFC3339)
 
 	calendarService, err := createCalService(ctx)
 	//
 	if err != nil {
-		log.Fatalf("Error while creating calendar service")
+		return nil, fmt.Errorf("error while creating calendar service: %w", err)
 	}
 	events, err := calendarService.Events.List("primary").ShowDeleted(false).
 		SingleEvents(true).TimeMin(t).MaxResults(int64(limit)).OrderBy("startTime").Do()
 
 	if err != nil {
-		log.Fatalf("Unable to retrieve next ten of the user's events: %v", err)
+		return nil, fmt.Errorf("unable to retrieve next ten of the user's events: %w", err)
 	}
-	log.Println("Upcoming events:")
 
-	event_slice := make([]string, limit)
-	if len(events.Items) == 0 {
-		log.Println("No upcoming events found.")
-	} else {
+	eventList := make([]string, 0, len(events.Items))
 
-		for index := range limit {
-			item := events.Items[index]
+	for _, item := range events.Items {
 
-			date := item.Start.DateTime
-			if date != "" {
-				t, err := time.Parse(time.RFC3339, date)
-				if err != nil {
-					log.Fatalf("Error while parsing time %v", err)
-				}
-				date = t.Format("02-01-2006 03:04:05 PM")
-			} else {
-				date = item.Start.Date
+		date := item.Start.DateTime
+		if date != "" {
+			t, err := time.Parse(time.RFC3339, date)
+			if err != nil {
+				return nil, fmt.Errorf("error while parsing time: %w", err)
 			}
-			value := fmt.Sprintf("%v (%v)\n", item.Summary, date)
-			if len(value) == 0 {
-				continue
-			}
-			event_slice[index] = value
+			date = t.Format("02-01-2006 03:04:05 PM")
+		} else {
+			date = item.Start.Date
+		}
+		value := fmt.Sprintf("%s — %s", item.Summary, date)
+		eventList = append(eventList, value)
+	}
+
+	return eventList, nil
+}
+
+func createCalService(ctx context.Context) (*calendar.Service, error) {
+	token, err := login(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("logging in: %w", err)
+	}
+	calendarService, err := calendar.NewService(ctx, option.WithTokenSource(config.TokenSource(ctx, token)))
+	//
+	if err != nil {
+		return nil, fmt.Errorf("creating calendar service: %w", err)
+	}
+	return calendarService, nil
+}
+
+// ====================
+// OAuth Authentication
+// ====================
+func login(ctx context.Context) (*oauth2.Token, error) {
+	configdir, _ := os.UserConfigDir()
+
+	refreshTokenPath := filepath.Join(configdir, "sechduler", "refresh_token.txt")
+
+	schedulerConfigDir := filepath.Join(configdir, "sechduler")
+	_, err := os.Stat(schedulerConfigDir)
+
+	if os.IsNotExist(err) {
+		err := os.Mkdir(schedulerConfigDir, 0755)
+
+		if err != nil {
+			return nil, err
 		}
 	}
-	return event_slice
+
+	refreshTokenData, rerr := os.ReadFile(refreshTokenPath)
+
+	if rerr == nil {
+
+		initToken := &oauth2.Token{
+			RefreshToken: string(refreshTokenData),
+		}
+
+		ts := config.TokenSource(ctx, initToken)
+		token, err := ts.Token()
+
+		if err != nil {
+			return nil, err
+		}
+
+		rerr := os.WriteFile(refreshTokenPath, []byte(token.RefreshToken), 0644)
+
+		if rerr != nil {
+			return nil, rerr
+		}
+
+		log.Printf("Logged in successfully")
+		return token, nil
+
+	}
+
+	url := config.AuthCodeURL("state")
+
+	log.Println(url)
+	log.Println("Paste the authorization code:")
+
+	var code string
+	_, serr := fmt.Scan(&code)
+
+	if serr != nil {
+		return nil, serr
+	}
+	token, err := config.Exchange(ctx, code)
+	if err != nil {
+		return nil, err
+	}
+	rerr = os.WriteFile(refreshTokenPath, []byte(token.RefreshToken), 0644)
+	if rerr != nil {
+		return nil, rerr
+	}
+
+	return token, nil
 }
 
 func main() {
@@ -255,14 +282,14 @@ func main() {
 	exeDir := filepath.Dir(exe)
 	file := filepath.Join(exeDir, ".env")
 
-	if _, err := os.Stat(file); err == nil {
+	if _, err := os.Stat(file); !os.IsNotExist(err) {
 
 		if err := godotenv.Load(file); err != nil {
-			log.Fatalf("Error while loading env file %v", err)
+			log.Fatalf("error while loading env file %v", err)
 			return
 		}
 
-		log.Printf("Env file loaded sucessfully")
+		log.Printf("Env file loaded successfully")
 
 	} else {
 
@@ -272,12 +299,12 @@ func main() {
 
 	}
 
-	google_client_id := os.Getenv("GOOGLE_CLIENT_ID")
-	google_client_secret := os.Getenv("GOOGLE_CLIENT_SECRET")
+	googleClientID := os.Getenv("GOOGLE_CLIENT_ID")
+	googleClientSecret := os.Getenv("GOOGLE_CLIENT_SECRET")
 
 	config = &oauth2.Config{
-		ClientID:     google_client_id,
-		ClientSecret: google_client_secret,
+		ClientID:     googleClientID,
+		ClientSecret: googleClientSecret,
 		Endpoint:     google.Endpoint,
 		RedirectURL:  "urn:ietf:wg:oauth:2.0:oob",
 		Scopes:       []string{"https://www.googleapis.com/auth/calendar"},
@@ -294,15 +321,15 @@ func main() {
 	}, addEvent)
 
 	mcp.AddTool(server, &mcp.Tool{
-		Name:        "listEvents",
+		Name:        "listUpcomingEvents",
 		Description: "Lists all the future events from now",
-	}, listEventsByLimit)
+	}, listUpcomingEventsByLimit)
 	mcp.AddTool(server, &mcp.Tool{
 		Name:        "deleteEventByNameAndDate",
 		Description: "Deletes the event by name and start and end date",
 	}, deleteEventByName)
 
-	if xerr := server.Run(context.Background(), &mcp.StdioTransport{}); err != nil {
-		log.Fatal(xerr)
+	if err := server.Run(context.Background(), &mcp.StdioTransport{}); err != nil {
+		log.Fatal(err)
 	}
 }
